360224a80c3c898ba65a999ff9858799e4f53836
Tran Bao Phuc <felixtran2000@gmail.com>
2021-08-28 20:44:21 +0700

    init

---
+++ test.py
@@ -0,0 +1,17 @@
+import requests
+import csv
+import json
+import time
+
+url = "http://www.boredapi.com/api/activity?"
+
+def collectData(amount=1000, interval=1,):
+    f = csv.writer(open("test.csv", "a+"))
+    for i in range(amount):
+        time.sleep(interval)
+        response = requests.get(url)
+        res = response.json()
+        f.writerow([res['key'], res["activity"], res["accessibility"], res["type"], res["participants"], res['price'], res['link']]) 
+
+
+collectData(1000)
